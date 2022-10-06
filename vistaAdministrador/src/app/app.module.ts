0ca8b7a989c9540d745260c19388f01cec97ff1b
Zamo98 <guzamora@estudiantec.cr>
2022-10-06 00:05:23 -0600

    Creación de componentes login y principal

--- vistaAdministrador/src/app/app.module.ts
+++ vistaAdministrador/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PrincipalComponent } from './principal/principal.component';
 
+const appRoutes:Routes=[
+  {path: '', component:PrincipalComponent},
+  {path: 'login', component:LoginComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PrincipalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]

b38fc2f6346b6b706b91fe89394836debd40bd2a
yaritzaelena <48070919+yaritzaelena@users.noreply.github.com>
2022-10-07 23:36:49 -0600

    Componente de Repuestos e insumos

--- vistaAdministrador/src/app/app.module.ts
+++ vistaAdministrador/src/app/app.module.ts
@@ -5,16 +5,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { PrincipalComponent } from './principal/principal.component';
+import { InsumoComponent } from './insumo/insumo.component';
 
 const appRoutes:Routes=[
   {path: '', component:PrincipalComponent},
-  {path: 'login', component:LoginComponent}
+  {path: 'login', component:LoginComponent},
+  {path:'insumo', component:InsumoComponent}
 ]
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    InsumoComponent
   ],
   imports: [
     BrowserModule,

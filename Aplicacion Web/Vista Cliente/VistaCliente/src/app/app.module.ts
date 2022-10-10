4780f0de19487dc13e83dc1252a46ddb17124629
Axel Cordero <axeljcm@estudiantec.cr>
2022-10-09 23:05:20 -0600

    login incompleto ( creacion de componentes de menu y gest de puntos)

--- Aplicacion Web/Vista Cliente/VistaCliente/src/app/app.module.ts
+++ Aplicacion Web/Vista Cliente/VistaCliente/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MenuclienteComponent } from './menucliente/menucliente.component';
+import { GestpuntosComponent } from './gestpuntos/gestpuntos.component';
+
+const appRoutes:Routes=[
+  {path: '', component:LoginComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MenuclienteComponent,
+    GestpuntosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

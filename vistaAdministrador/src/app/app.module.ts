a1789e0c024b3f4a166a36a0096eb0422680afec
Gustavo Zamora <guzamora@estudiantec.cr>
2022-10-10 22:30:39 -0600

    Corrección

--- vistaAdministrador/src/app/app.module.ts
+++ vistaAdministrador/src/app/app.module.ts
@@ -6,49 +6,31 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { PrincipalComponent } from './principal/principal.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { TrabajadoresComponent } from './trabajadores/trabajadores.component';
 import { SucursalesComponent } from './sucursales/sucursales.component';
 import { ProveedoresComponent } from './proveedores/proveedores.component';
-=======
 import { InsumoComponent } from './insumo/insumo.component';
->>>>>>> Vista-Administrador
-=======
-import { InsumoComponent } from './insumo/insumo.component';
->>>>>>> Vista-Administrador
 
 const appRoutes:Routes=[
   {path: '', component:PrincipalComponent},
   {path: 'login', component:LoginComponent},
-<<<<<<< HEAD
-<<<<<<< HEAD
   {path: 'trabajadores', component:TrabajadoresComponent},
   {path: 'sucursales', component:SucursalesComponent},
-  {path: 'proveedores', component:ProveedoresComponent}
-=======
-  {path:'insumo', component:InsumoComponent}
->>>>>>> Vista-Administrador
-=======
-  {path:'insumo', component:InsumoComponent}
->>>>>>> Vista-Administrador
+  {path: 'proveedores', component:ProveedoresComponent},
+  {path: 'insumo', component:InsumoComponent}
+  
 ]
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     PrincipalComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
     TrabajadoresComponent,
     SucursalesComponent,
-    ProveedoresComponent
-=======
-    InsumoComponent
->>>>>>> Vista-Administrador
-=======
-    InsumoComponent
->>>>>>> Vista-Administrador
+    ProveedoresComponent,
+    InsumoComponent,
+
   ],
   imports: [
     BrowserModule,

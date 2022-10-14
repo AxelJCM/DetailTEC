0f08337746a290aa5543d3cd0a51f561f605378c
Axel Cordero <axeljcm@estudiantec.cr>
2022-10-13 18:46:18 -0600

    +cambios vista cliente

--- Aplicacion Web/Vista Cliente/VistaCliente/src/app/app.module.ts
+++ Aplicacion Web/Vista Cliente/VistaCliente/src/app/app.module.ts
@@ -9,9 +9,10 @@ import { MenuclienteComponent } from './menucliente/menucliente.component';
 import { GestpuntosComponent } from './gestpuntos/gestpuntos.component';
 
 const appRoutes:Routes=[
-  {path: '', component:LoginComponent}
+  {path: 'login', component:LoginComponent}
 ,{path: 'menucliente', component:MenuclienteComponent}
-,{path: '', component:GestpuntosComponent}
+,{path: 'gestpuntos', component:GestpuntosComponent},
+{path: '', redirectTo: '/login', pathMatch: 'full'}
 ]
 
 @NgModule({
